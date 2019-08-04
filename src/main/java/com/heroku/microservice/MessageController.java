5da5ba90dce39727297d15e5818731c634a30e91
git4mahadi <53706421+git4mahadi@users.noreply.github.com>
2019-08-04 23:03:23 +0600

    Add files via upload

---
+++ src/main/java/com/heroku/microservice/MessageController.java
@@ -0,0 +1,21 @@
+package com.heroku.microservice;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class MessageController {
+
+    @GetMapping("/message")
+    public String message() {
+        return "As-Salamu-Alikum";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello ".concat(name);
+    }
+}
